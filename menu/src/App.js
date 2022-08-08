d2b37b4f1a333dcbad2fddb5b6d500a71cd7bd63
BruceDorsey <bruce.dorsey@cdpipelinedevshop.com>
2022-08-08 17:04:35 +0000

    menu

---
+++ menu/src/App.js
@@ -0,0 +1,13 @@
+import Album from '/workspace/Food-Menu-2/menu/src/components/Album.js';
+import './App.css';
+
+function App() {
+  return (
+  <div className="App" >
+  <Album></Album>
+</div>
+
+  );
+}
+
+export default App;
